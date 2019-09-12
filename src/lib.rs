2552039bdc0bdf8e7a2f0a8337bdafc090a46d93
Sven Assmann <sven.assmann.it@gmail.com>
2019-09-12 23:57:47 +0200

    feat(BitIterator): implemented
     - tests for little endian are done
     - bit iterator does what it should

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,12 @@
-use image::{GenericImageView, ImageBuffer, Pixel, Rgba};
+use image::*;
+use std::borrow::BorrowMut;
 use std::fs::File;
-use std::io;
 use std::io::prelude::*;
+use std::io::BufReader;
+use std::io::Bytes;
+use std::io::*;
 use std::path::Path;
+use std::slice;
 
 pub struct Steganogramm {
     target: Option<String>,
@@ -11,11 +15,99 @@ pub struct Steganogramm {
 }
 
 pub trait Encoder {
-    fn hide(self) -> Self;
+    fn hide(&mut self) -> &mut Self;
 }
 
 pub trait Decoder {
     fn unhide(self) -> Self;
+}
+
+pub struct BitIterator<I> {
+    n: u32,
+    i: u32,
+    iter: I,
+    byte: Option<u8>,
+}
+
+impl<I> BitIterator<I> {
+    pub fn new(s: I) -> Self {
+        BitIterator {
+            n: 8,
+            i: 0,
+            iter: s,
+            byte: None,
+        }
+    }
+}
+
+impl<I: Read> Iterator for BitIterator<I> {
+    type Item = u8;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            let bit = self.i % self.n;
+            if bit == 0 {
+                self.byte = None;
+            }
+            self.i = self.i + 1;
+            return match self.byte {
+                None => {
+                    let mut b = 0;
+                    match self.iter.read(slice::from_mut(&mut b)) {
+                        Ok(0) => None,
+                        Ok(..) => {
+                            self.byte = Some(b);
+                            Some((b >> bit) & 1)
+                        }
+                        Err(ref e) if e.kind() == ErrorKind::Interrupted => continue,
+                        Err(_) => None,
+                    }
+                }
+                Some(b) => Some((b >> bit) & 1),
+            };
+        }
+    }
+}
+
+impl Encoder for Steganogramm {
+    fn hide(&mut self) -> &mut Steganogramm {
+        let source = self.source.take().unwrap();
+        let carrier = self.carrier.take().unwrap();
+        let (width, heigh) = carrier.dimensions();
+        let bit_iter = BitIterator::new(source);
+
+        let mut target: RgbaImage = ImageBuffer::new(width, heigh);
+
+        for (x, y, pixel) in target.enumerate_pixels_mut() {
+            let other = carrier.get_pixel(x, y);
+            // let r = (0.3 * x as f32) as u8;
+            // let b = (0.3 * y as f32) as u8;
+            // TODO feat(core:hide) implement the basic functionality here
+            let image::Rgba(data) = other;
+            *pixel = Rgba([data[0], data[1], data[2], data[3]]);
+        }
+        target.save(self.target.take().unwrap()).unwrap();
+
+        // loop {
+        //   let mut buffer = [0; 512];
+        //   let bytes_read = source.read(&mut buffer).unwrap();
+
+        //   if bytes_read != buffer.len() {
+        //     break;
+        //   }
+        // }
+
+        // match self.encoder {
+        //   None => {},
+        //   Some(ref _enc) => {
+        //     // let data = [0x15; 512*512]; // An array containing a RGBA sequence. First pixel is red and second pixel is black.
+        //     // enc.write_header().unwrap().write_image_data(&data).unwrap(); // Save
+        //     // self.encoder = Some(*enc)
+        //   }
+        // }
+
+        self
+    }
 }
 
 impl Steganogramm {
@@ -40,44 +132,6 @@ impl Steganogramm {
 
     pub fn take_data_to_hide_from(&mut self, input_file: &str) -> &mut Steganogramm {
         self.source = Some(File::open(input_file).expect("Source file was not readable."));
-        self
-    }
-
-    pub fn hide(&mut self) -> &mut Steganogramm {
-        let mut source = self.source.take().unwrap();
-        let carrier = self.carrier.take().unwrap();
-        let (width, heigh) = carrier.dimensions();
-
-        let mut target: image::RgbaImage = image::ImageBuffer::new(width, heigh);
-
-        for (x, y, pixel) in target.enumerate_pixels_mut() {
-            let other = carrier.get_pixel(x, y);
-            let r = (0.3 * x as f32) as u8;
-            let b = (0.3 * y as f32) as u8;
-            // TODO feat(core:hide) implement the basic functionality here
-            *pixel = Rgba([r, 0, b, 0xff]);
-            // *pixel = other;
-        }
-        target.save(self.target.take().unwrap()).unwrap();
-
-        // loop {
-        //   let mut buffer = [0; 512];
-        //   let bytes_read = source.read(&mut buffer).unwrap();
-
-        //   if bytes_read != buffer.len() {
-        //     break;
-        //   }
-        // }
-
-        // match self.encoder {
-        //   None => {},
-        //   Some(ref _enc) => {
-        //     // let data = [0x15; 512*512]; // An array containing a RGBA sequence. First pixel is red and second pixel is black.
-        //     // enc.write_header().unwrap().write_image_data(&data).unwrap(); // Save
-        //     // self.encoder = Some(*enc)
-        //   }
-        // }
-
         self
     }
 }
--- tests/stegano_spec.rs
+++ tests/stegano_spec.rs
@@ -1,6 +1,6 @@
 use speculate::speculate;
 use std::fs;
-use stegano::Steganogramm;
+use stegano::{BitIterator, Encoder, Steganogramm};
 
 speculate! {
     describe "Steganogramm::new()" {
@@ -20,7 +20,7 @@ speculate! {
         it "should hide the Cargo.toml on a png carrier to a new png file" {
             Steganogramm::new()
                 .take_data_to_hide_from("Cargo.toml")
-                .use_carrier_image("ressources/HelloWorld_no_passwd_v2.x.png")
+                .use_carrier_image("resources/HelloWorld_no_passwd_v2.x.png")
                 .write_to("/tmp/out-test-image.png")
                 .hide();
 
@@ -29,6 +29,42 @@ speculate! {
                 .len();
 
             assert_ne!(l, 0, "Output image was empty.");
+        }
+    }
+    describe "BitIterator::next()" {
+        // String: H           e           l
+        // Hex   : 0x48        0x61        0x6C
+        // Binary: 0b01001000  0b01100001  0b01101100
+        it "should return the 8 bits of 'H' in LittleEndian byte order" {
+            let b = vec![0b01001000, 0b01100001];
+            let mut it = BitIterator::new(&b[..]);
+
+            assert_eq!(it.next().unwrap(), 0, "1st bit not correct");
+            assert_eq!(it.next().unwrap(), 0, "2nd bit not correct");
+            assert_eq!(it.next().unwrap(), 0, "3rd bit not correct");
+            assert_eq!(it.next().unwrap(), 1, "4th bit not correct");
+            assert_eq!(it.next().unwrap(), 0, "5th bit not correct");
+            assert_eq!(it.next().unwrap(), 0, "6th bit not correct");
+            assert_eq!(it.next().unwrap(), 1, "7th bit not correct");
+            assert_eq!(it.next().unwrap(), 0, "8th bit not correct");
+        }
+
+        // String: H           e           l
+        // Hex   : 0x48        0x61        0x6C
+        // Binary: 0b01001000  0b01100001  0b01101100
+        it "should return 8 bits of 'e' in LittleEndian byte order after skip(8)" {
+            let b = vec![0b01001000, 0b01100001];
+            let mut it = BitIterator::new(&b[..]).skip(8);
+
+            assert_eq!(it.next().unwrap(), 1, "1st bit not correct");
+            assert_eq!(it.next().unwrap(), 0, "2nd bit not correct");
+            assert_eq!(it.next().unwrap(), 0, "3rd bit not correct");
+            assert_eq!(it.next().unwrap(), 0, "4th bit not correct");
+            assert_eq!(it.next().unwrap(), 0, "5th bit not correct");
+            assert_eq!(it.next().unwrap(), 1, "6th bit not correct");
+            assert_eq!(it.next().unwrap(), 1, "7th bit not correct");
+            assert_eq!(it.next().unwrap(), 0, "8th bit not correct");
+            assert_eq!(it.next(), None, "it should end after the last bit on the last byte");
         }
     }
 }
