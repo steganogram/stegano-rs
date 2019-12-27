76c7e8176d75105dbde7ec4e533fd5f55dc897e1
Sven Assmann <sven.assmann.it@gmail.com>
2019-12-27 16:48:46 +0100

    benchmark(decoder): add a benchmark for the ByteReader

---
+++ benches/byte_reader_benchmark.rs
@@ -0,0 +1,14 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use stegano::SteganoDecoderV3;
+use std::io::Read;
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("Stegano::ByteReader for resources/HelloWorld_no_passwd_v2.x.png", |b| b.iter(|| {
+        let mut dec = SteganoDecoderV3::new("resources/HelloWorld_no_passwd_v2.x.png");
+        let mut buf = Vec::new();
+        dec.read_to_end(&mut buf).unwrap();
+    }));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
