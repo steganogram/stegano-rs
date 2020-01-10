93287dc7ebb28ef6acdbbb1aad2f305b88da3fbc
Sven Assmann <sven.assmann.it@gmail.com>
2020-01-10 16:20:49 +0100

    feat(ci:coverage): switch to tarpaulin for coverage metrics

--- .ci/post-build/coverage.sh
+++ .ci/post-build/coverage.sh
@@ -1,19 +1,4 @@
 if [[ $COVERAGE ]]; then
-  set -e
-  wget https://github.com/SimonKagstrom/kcov/archive/master.tar.gz
-  tar xzf master.tar.gz
-  cd kcov-master
-  mkdir build
-  cd build
-  cmake ..
-  make
-  sudo make install
-  cd ../..
-  rm -rf kcov-master
-  for file in target/debug/*.d; do
-    mkdir -p "target/cov/$(basename $file)"
-    kcov --exclude-pattern=/.cargo,/usr/lib --verify "target/cov/$(basename $file)" "$file"
-  done
-  bash <(curl -s https://codecov.io/bash) &&
-  echo "Uploaded code coverage"
+    cargo tarpaulin --out Xml
+    bash <(curl -s https://codecov.io/bash)
 fi
