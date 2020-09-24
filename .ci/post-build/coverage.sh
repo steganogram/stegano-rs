5bd3cedb10f16f439719bd9a86f33b3ce4e774ca
Sven Assmann <sven.assmann.it@gmail.com>
2020-09-24 09:05:01 +0200

    chore(ci:travis): goodbye travis, it was a nice time with you
     - remove:
       - travis config
       - old ci related scripts

--- .ci/post-build/coverage.sh
+++
@@ -1,2 +0,0 @@
-# run on osx
-docker run --security-opt seccomp=unconfined -v "${PWD}:/volume" xd009642/tarpaulin
--- .ci/post-build/publish-docs.sh
+++
@@ -1,5 +0,0 @@
-if [[ $PUBLISH_DOCS ]]; then
-  cargo doc
-  echo '<meta http-equiv="refresh content=0;url=new_url/index.html">' > target/doc/index.html
-  echo 'git clone https://github.com/davisp/ghp-import.git && ./ghp-import/ghp_import.py -n -p -f -m "Documentation upload" -r https://"$GH_TOKEN"@github.com/"$TRAVIS_REPO_SLUG.git" target/doc && echo "Uploaded documentation"'
-fi
